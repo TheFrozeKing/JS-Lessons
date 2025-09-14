ac4afead3de61e651c350db894b338cec70daf13
TheFrozeKing <frozefama@mail.ru>
2025-09-14 20:42:37 +0300

    Added lesson 2

---
+++ Браузер/Часть 2/B/js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let cardsData = [
+    {
+        inStock: true,
+        imgUrl: 'system/img/choco.jpg',
+        text: 'Сливочно-кофейное с кусочками шоколада',
+        price: 310,
+        isHit: true,
+        specialOffer: 'Двойная порция сиропа бесплатно!'
+    },
+    {
+        inStock: false,
+        imgUrl: 'system/img/lemon.jpg',
+        text: 'Сливочно-лимонное с карамельной присыпкой',
+        price: 125,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'system/img/cowberry.jpg',
+        text: 'Сливочное с брусничным джемом',
+        price: 170,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'system/img/cookie.jpg',
+        text: 'Сливочное с кусочками печенья',
+        price: 250,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'system/img/creme-brulee.jpg',
+        text: 'Сливочное крем-брюле',
+        price: 190,
+        isHit: false
+    }
+];
