4add5952d2ea9f2b715861fb7a02a4bf603cff9b
StudGit6 <bebrabezadidasov@mail.ru>
2025-09-15 12:35:33 +0300

    finished lesson 3

--- Браузер/Часть 3/A/js/script.js
+++ Браузер/Часть 3/A/js/script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const createElement = (tagName, className, text = '', appendTo) =>{
+    let newElement = document.createElement(tagName);
+    newElement.classList.add(className);
+    newElement.textContent = text;
+
+    appendTo.appendChild(newElement);
+    if(appendTo){
+    }
+    return newElement;
+}
+
+const createImage = (className, imageUrl, alt, appendTo) => {
+    let image = createElement('img', className, '', appendTo);
+    image.src = imageUrl;
+    image.alt = alt;
+    if(appendTo){
+        appendTo.appendChild(image);
+    }
+}
+
+const initializeButtons = () => {
+    let popup = document.querySelector('.modal');
+    //popup.classList.add('modal--show');
+
+    let openButtons = document.querySelectorAll('.button-open');
+    openButtons.forEach(element => element.addEventListener('click', (evt) => {evt.preventDefault(); popup.classList.add('modal--show')}));
+
+    let closeButtons = document.querySelectorAll('.button-close');
+    closeButtons.forEach(element => element.addEventListener('click', (evt) => {evt.preventDefault(); popup.classList.remove('modal--show')}));
+    document.addEventListener('keydown', (key) => 
+        {
+            if(key.key === 'Escape'){
+                popup.classList.remove('modal--show');   
+            }
+        })
+}
+initializeButtons();
--- Браузер/Часть 3/B/js/script.js
+++ Браузер/Часть 3/B/js/script.js
@@ -1,3 +1,27 @@
 'use strict';
 
+const initializeButtons = () => {
+    const popup = document.querySelector('.popup');
+
+    const openButtons = document.querySelectorAll('.button-show');
+    openButtons.forEach(button => button.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        popup.classList.add('popup--open');
+    }));
+
+    const closeButtons = document.querySelectorAll('.button-hide');
+    closeButtons.forEach(button => button.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        popup.classList.remove('popup--open');
+    }));
+
+    document.addEventListener('keydown', key => {
+        if(key.key === 'Escape'){
+            popup.classList.remove('popup--open');
+        }
+    })
+};
+
+initializeButtons();
+
 
