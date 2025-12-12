f5095e810ff985562a6d90056bf974b36962b750
TheFrozeKing <frozefama@mail.ru>
2025-12-12 15:37:00 +0300

    Finished jspp1

---
+++ Мини-приложения/JSPP1-Calculator/app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const inputs = document.querySelectorAll('input');
+
+const performCalculations = (operationId) => {
+    const num1 = Number(inputs[0].value);
+    const num2 = Number(inputs[1].value);
+    inputs[2].value = "";
+
+    if(operationId === "add"){
+        inputs[2].value = num1+num2;
+    }
+    else if(operationId === "subtract"){
+         inputs[2].value = num1-num2;
+    }
+    else if(operationId === "multiply"){
+         inputs[2].value = num1*num2;
+
+    }
+    else if(operationId === "divide"){
+        if(num1 === 0 || num2 === 0){
+            alert('Нельзя делить на 0!');
+            return;
+        }
+        inputs[2].value = num1/num2;
+    }
+}
+
+const initializeButtons = () => {
+    const buttons = document.querySelectorAll('button');
+    
+    buttons.forEach((button) => {
+        button.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            performCalculations(button.id);
+        });
+    });
+}
+
+initializeButtons();
+
